b722355d99267759739806972419a07c8c0e1a23
Mahmood shariff roddam <126786374+mahmoodshariff123@users.noreply.github.com>
2024-03-05 19:57:28 +0530

    Update ATM_INTERFACE.java

--- ATM_INTERFACE.java
+++ ATM_INTERFACE.java
@@ -89,3 +89,10 @@ class ATM {
         account.withdraw(amount);
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        BankAccount userAccount = new BankAccount(1000);
+        ATM atm = new ATM(userAccount);
+        atm.start();
+    }
+}
